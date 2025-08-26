2a08c873539b336d7e8452aee35c2c35d3d96cc0
threekd <threekd42@gmail.com>
2025-08-27 04:54:58 +0800

    update

--- kehua/main.py
+++ kehua/main.py
@@ -6,9 +6,9 @@ from appium.options.android import UiAutomator2Options
 desired_caps = {
   'platformName': 'Android', # 被测手机是安卓
   'platformVersion': '15', # 手机安卓版本，如果是鸿蒙系统，依次尝试 12、11、10 这些版本号
-  'deviceName': 'Xiaomi 15', # 设备名，安卓手机可以随意填写
-  'appPackage': 'tv.danmaku.bili', # 启动APP Package名称
-  'appActivity': '.MainActivityV2', # 启动Activity名称
+  'deviceName': 'xxx', # 设备名，安卓手机可以随意填写
+  'appPackage': 'com.app.tideswing', # 启动APP Package名称
+  'appActivity': '.ui.SplashActivity', # 启动Activity名称
   'unicodeKeyboard': True, # 自动化需要输入中文时填True
   'resetKeyboard': True, # 执行完程序恢复原来输入法
   'noReset': True,       # 不要重置App
@@ -22,30 +22,9 @@ driver = webdriver.Remote('http://localhost:4723/wd/hub',
   options=UiAutomator2Options().load_capabilities(desired_caps))
 
 # 设置缺省等待时间
-driver.implicitly_wait(5)
+driver.implicitly_wait(2)
 
-# 如果有`青少年保护`界面，点击`我知道了`
-iknow = driver.find_elements(By.ID, "text3")
-if iknow:
-    iknow.click()
 
-# 根据id定位搜索位置框，点击
-driver.find_element(By.ID, 'expand_search').click()
-
-# 根据id定位搜索输入框，点击
-sbox = driver.find_element(By.ID, 'search_src_text')
-sbox.send_keys('白月黑羽')
-# 输入回车键，确定搜索
-driver.press_keycode(AndroidKey.ENTER)
-
-# 选择（定位）所有视频标题
-eles = driver.find_elements(By.ID, 'title')
-
-for ele in eles:
-    # 打印标题
-    print(ele.text)
 
 input('**** Press to quit..')
 driver.quit()
-
-
